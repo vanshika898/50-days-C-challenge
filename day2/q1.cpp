881efdd3625469123f305774f139d94f62f0e340
vanshika nigam <140736318+vanshika898@users.noreply.github.com>
2023-08-16 16:24:04 +0000

    main

---
+++ day2/q1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+class print{
+   public: void name(){
+        cout<<"vanshika";
+    }
+    
+    
+};
+int main(){
+    print o;
+    o.name();
+    return 0;
+    
+}
