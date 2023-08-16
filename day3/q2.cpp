846629c0f28231480823f92a258ea2246c3a6ea5
vanshika nigam <140736318+vanshika898@users.noreply.github.com>
2023-08-16 17:19:52 +0000

    main

---
+++ day3/q2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+class franchise{
+    public:
+    void kfc(){
+        cout<<"we have best chiken"<<endl;
+    }
+    public:void burgerking(){
+        cout<<"we have best burgers"<<endl;
+    }
+};
+int main(){
+    franchise o;
+    o.kfc();
+    o.burgerking();
+    
+}
