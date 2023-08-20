dc2bbbe88a4c4bbfe93ce601a81665d2533f7ecb
vanshika nigam <140736318+vanshika898@users.noreply.github.com>
2023-08-20 10:35:11 +0000

    main

---
+++ day5/q2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int number[]={1,2,3,4,5};
+    //for(int i:number){//for each loop
+        int getlength=sizeof(number)/sizeof(int);
+        cout<<getlength<<endl;
+
+
+    }
+
