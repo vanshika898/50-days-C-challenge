179afb0b3d5a77a5ec3c5cce4803d7af841c9525
vanshika nigam <140736318+vanshika898@users.noreply.github.com>
2023-08-20 13:57:07 +0000

    main

---
+++ day6/q2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class parent{
+    public:
+    void method();
+
+};
+void parent::method(){
+    cout<<"hello This vanshika!";
+}
+int main(){
+ parent o;
+ o.method();
+ return 0;
+    
+
+    }
+
