bf4767a801cf70ee20c1233874add9b2b5f8a894
vanshika nigam <140736318+vanshika898@users.noreply.github.com>
2023-08-20 09:49:31 +0000

    main

---
+++ day4/q1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int day;
+    cout<<"enter the day number\n";
+    cin>>day;
+    switch(day){
+        case 1:
+        cout<<"monday"<<endl;
+        break;
+        case 2:
+        cout<<"Tuesday"<<endl;
+        break;
+        case 3:
+        cout<<"wednesdyay"<<endl;
+        break;
+        case 4:
+        cout<<"thursday"<<endl;
+        break;
+        case 5:
+        cout<<"friday"<<endl;
+        break;
+        case 6:
+        cout<<"saturday"<<endl;
+        break;
+        case 7:
+        cout<<"sunday"<<endl;
+        break;
+        
+
+    }
+    return 0;
+    }
+
