9dd48987b5ce9a6c6df044dd5888cba7303c7f03
vanshika nigam <140736318+vanshika898@users.noreply.github.com>
2023-08-20 10:44:51 +0000

    main

---
+++ day5/q3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int number[2][3]={{1,2,3},{2,3,4}};
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+            cout<<number[i][j]<<"\n";
+        }
+
+    }
+    return 0;
+    }
+
